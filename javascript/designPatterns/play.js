76ba3536bc60f3cba51154816905d0c775cccc6a
sparky.dasrath <sdasrath@gmail.com>
2018-06-04 21:57:55 -0400

    jQuery lesson 1 - show, hide, toggle

--- javascript/designPatterns/play.js
+++ javascript/designPatterns/play.js
@@ -1,17 +1,22 @@
 
-var button = document.getElementById('playButton');
+// var button = document.getElementById('playButton');
 
-console.log("context before click")
-console.log(this.toString())
+// console.log("context before click")
+// console.log(this.toString())
 
-button.addEventListener('click', handleClick.bind(this));
+// button.addEventListener('click', handleClick.bind(this));
 
 
 
-function handleClick(event)
-{
+// function handleClick(event)
+// {
     
-    console.log('In click event');
-    console.log('Context in click event');
-    console.log(this.toString());
-}
+//     console.log('In click event');
+//     console.log('Context in click event');
+//     console.log(this.toString());
+// }
+
+
+var body = document.getElementById('b');
+console.log(body.outerHTML);
+console.log(body.innerHTML);
