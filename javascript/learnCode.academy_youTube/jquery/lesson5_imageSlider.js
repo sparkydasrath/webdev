e8d4e514ec5a0a649468807d9fbff01edfebff4c
sparky.dasrath <sdasrath@gmail.com>
2018-06-05 15:08:43 -0400

    jQuery lesson 5 end
    https://www.youtube.com/watch?v=hMxGhHNOkCU&list=PLoYCgNOIyGABdI2V8I_SWo22tFpgh2s6_

--- javascript/learnCode.academy_youTube/jquery/lesson5_imageSlider.js
+++ javascript/learnCode.academy_youTube/jquery/lesson5_imageSlider.js
@@ -1,40 +1,48 @@
 'use strict';
 
-$(function(){
+$(function () {
 
     // config
     var width = 720;
     var animationTime = 1000;
-    var intervalTime = 1000;
+    var intervalTime = 2000;
     var currentSlide = 1;
 
     // cacheDOM
     var $slider = $('#slider');
     var $slideContainer = $slider.find('.slides');
     var $slides = $slideContainer.find('.slide')
-    
+
     var interval;
 
-
-
     // setInterval
-    
-    
-    setInterval(function(){
-        $($slideContainer).animate({'margin-left': '-='+width}, animationTime, function(){
-            currentSlide++;
-            if (currentSlide === $slides.length)
-            {
-                currentSlide = 1;
-                $slideContainer.css('margin-left', 0);
-            }
-        }) 
-    }, intervalTime);
-        // animate margin-left of slides
-        // if it's last slide, go to position 1 (0px)
-    
-       // listen for mouseenter to pause
-       // listen for mouseleave to resume 
+    function startSlider() {
+        interval = setInterval(function () {
+            $($slideContainer).animate({
+                'margin-left': '-=' + width
+            }, animationTime, function () {
+                currentSlide++;
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            })
+        }, intervalTime);
+    }
 
-    $slider.on('mouseenter', pauseSlider).on('mouseLeave', startSlider);
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+    startSlider();
+
+
+    // animate margin-left of slides
+    // if it's last slide, go to position 1 (0px)
+
+    // listen for mouseenter to pause
+    // listen for mouseleave to resume 
+
 });
