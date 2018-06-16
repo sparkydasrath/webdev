66a8b140d7b7a6ef2f8db1f3fb2009b46ef5c3b8
sparky.dasrath <sdasrath@gmail.com>
2018-06-15 21:35:00 -0400

    Todo v1
    - created TodoItem
    - refactored displayListItems to show the contents of the new TodoItem

--- todo/v1/public/js/app.js
+++ todo/v1/public/js/app.js
@@ -1,14 +1,37 @@
 "use strict";
 
+/* BAD: create a montonically increasing global variable to store the id 
+    each time the app is run, the list will start at 0 since no storage is involved in this version
+*/ 
+
+let itemId = -1;
+
+window.onload = e => {console.log("window loaded " + e)}
 
 // cache dom elements
 let addTodoButton = document.getElementById("add-todo");
+let deleteTodoButton = document.getElementById("delete-todo");
 let inputTextbox = document.getElementById("input-text");
 let listItems = document.getElementById("list-items");
 let diag = document.getElementById("diag");
 
-function getTodoText()
-{
+// object to represent each toditem
+// use constructor function to pass along an id and the text on creation
+function TodoItem(id, todoText) {
+    this.id = id;
+    this.todoText = todoText;
+    this.isCompleted = false;
+
+    this.toString = function () {
+        return this.todoText;
+    }
+}
+
+function createTodoItem(id, text) {
+    return new TodoItem(id, text);
+}
+
+function getTodoText() {
     return inputTextbox.value;
 }
 
@@ -20,15 +43,23 @@ let todoList = [];
 
 
 function handleAddTodoButtonOnClick(eventArgs) {
-    console.log("button clicked");
 
-    todoList.push(getTodoText());
+    // increment the counter
+    // by virtue of this design, each id will match the correct index in the array used to store items, so deleting will be simple
+    itemId++; 
+
+    let todoItemToAdd = createTodoItem(itemId, getTodoText());
+    todoList.push(todoItemToAdd);
 
     listItems.innerHTML = "";
     listItems.innerHTML = displayListItems(todoList);
 }
 
-function displayListItems(listToShow)
-{
-    return listToShow.join();
+function displayListItems(listToShow) {
+    let result = "";
+    listToShow.forEach(element => {
+        let val = element.toString();
+        result += val;
+    });
+    return result;
 }
--- todo/v1/readme.md
+++ todo/v1/readme.md
@@ -1,10 +1,5 @@
 # TODO Version 1
 
-I will be using just HTML, CSS and JS to create the main functionality. Using the operations of [TodoMvc](http://todomvc.com/examples/).
+Tasks are tracked on [Trello](https://trello.com/b/NLNokVdL) 
 
-## TASKS
-| No    | Description   | Status    |
-| ------|:-------------:| -----:    |
-| 1     | Create basic project structure    | done |
-| 2     | Create HTML to represent Todo list| done |
-| 3     | are neat      |    $1     |
+I will be using just HTML, CSS and JS to create the main functionality. Using the operations of [TodoMvc](http://todomvc.com/examples/).
