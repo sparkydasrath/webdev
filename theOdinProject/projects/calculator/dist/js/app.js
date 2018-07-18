b7713de794afe60b76baecb0eb5829cf778cd329
sparky.dasrath <sdasrath@gmail.com>
2018-07-18 13:31:27 -0400

    The Odin Project - Calculator Project
    - LOTS of headache trying to get browerfiy and gulp to work - finally working

---
+++ theOdinProject/projects/calculator/dist/js/app.js
@@ -0,0 +1,81 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Ops {
+    add(x, y) {
+        return x + y;
+    }
+    subtract(x, y) {
+        return x - y;
+    }
+    multiply(x, y) {
+        return x * y;
+    }
+    divide(x, y) {
+        if (y === 0)
+            return 0;
+        return x / y;
+    }
+    negate(x) {
+        return (-1) * x;
+    }
+}
+exports.default = Ops;
+},{}],2:[function(require,module,exports){
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Ops_1 = __importDefault(require("./Ops"));
+class Dom {
+}
+class MainView {
+    constructor() {
+        this.clickEvent = "click";
+        this.buttonContainer = ".btn-container";
+        this.divisionButton = "#btn-div";
+        this.dom = new Dom();
+        this.ops = new Ops_1.default();
+    }
+    cacheDom() {
+        this.dom.buttonContainer = document.querySelector(this.buttonContainer);
+        this.dom.divisionButton = document.getElementById(this.divisionButton);
+    }
+    bindEvents() {
+        if (this.dom.buttonContainer === null || this.dom.buttonContainer === undefined) {
+            return;
+        }
+        this.dom.buttonContainer.addEventListener(this.clickEvent, this.handleBtnContainerClick);
+        if (this.dom.divisionButton === null || this.dom.divisionButton === undefined) {
+            return;
+        }
+        this.dom.divisionButton.addEventListener(this.clickEvent, this.handleDivisionButtonClicked);
+    }
+    handleBtnContainerClick(event) {
+        let src = event.srcElement;
+        if (src === null || src === undefined) {
+            console.log("cant find button src");
+        }
+        console.log("container clicked");
+    }
+    handleDivisionButtonClicked(event) {
+        console.log("division clicked");
+        if ((event === null || event === undefined) ||
+            (event.srcElement === null || event.srcElement === undefined))
+            return;
+        let res = this.ops.divide(4, 2);
+        console.log(res);
+    }
+    init() {
+        this.cacheDom();
+        this.bindEvents();
+    }
+}
+(function () {
+    let main = new MainView();
+    main.init();
+}());
+},{"./Ops":1}]},{},[2])
+
+//# sourceMappingURL=app.js.map
--- theOdinProject/projects/calculator/gulpfile.js
+++ theOdinProject/projects/calculator/gulpfile.js
@@ -2,36 +2,109 @@ let gulp = require("gulp");
 let gts = require("gulp-typescript");
 let mocha = require("gulp-mocha");
 let del = require("del");
-let sm = require("gulp-sourcemaps");
-
+let sourcemaps = require("gulp-sourcemaps");
+let log = require("fancylog");
+let uglify = require("gulp-uglify");
+let watchify = require("watchify");
+let browserify = require("browserify");
+let glob = require("glob");
+let source = require('vinyl-source-stream');
+let tsify = require("tsify");
+let buffer = require("vinyl-buffer");
 let tsProject = gts.createProject("tsconfig.json");
-let testProject = gts.createProject("tsconfig.json");
+
+let paths = {
+    "htmlSource": "src/views/*.html",
+    "cssSource": "src/css/*.css",
+    "specs": "test/ts/*.spec.ts",
+    "deleteDestination": "dist/**/*",
+    "globEntries": "./src/ts/*.ts",
+}
+
+let config = {
+    paths: {
+        "htmlSource": "src/views/*.html",
+        "cssSource": "src/css/*.css",
+        "specs": "test/ts/*.spec.ts",
+        "deleteDestination": "dist/**/*",
+    },
+    ext: {
+        glob: {
+            "globEntries": "./src/ts/*.ts",
+        },
+        browserifyConfig: {
+            basedir: ".",
+            debug: true,
+            entries: ["./src/ts/app.ts"] // main ts entry point
+        },
+        sourceMapsConfig: {
+            loadMaps: true,
+        }
+    }
+};
+
+// let fileList = glob.sync(config.paths.ext.glob.globEntries);
+
+// let browserifyConfig = {
+//     basedir: ".",
+//     debug: true,
+//     entries: [fileList]
+// }
+
+// let sourceMapsConfig = {
+//     loadMaps: true,
+// }
 
 gulp.task("clean", () => {
-    return del(["dist/**/*"]);
+    return del([paths.deleteDestination]);
 });
 
-// gulp.task("mocha-sidebar-tests", () => {
-//     return testProject.src()
-//         .pipe(testProject())
-//         .js
-//         .pipe(gulp.dest("test/js"));
-// });
 
 gulp.task("run-tests", function () {
-    return gulp.src("test/ts/*.spec.ts")
+    return gulp.src(paths.specs)
         .pipe(mocha({
             reporter: "list",
             require: ["ts-node/register"]
         }));
 });
 
+// gulp.task("compile", () => {
+
+//     let bify = browserify(browserifyConfig);
+
+//     return tsProject
+//         .src()
+//         .pipe(tsProject())
+//         .js
+//         .pipe(gulp.dest("dist"));
+// });
+
 gulp.task("compile", () => {
-    return tsProject
-        .src()
-        .pipe(tsProject())
-        .js
-        .pipe(gulp.dest("dist/js"));
+
+    // let bify = browserify(browserifyConfig);
+    let bify = browserify(config.ext.browserifyConfig);
+
+    return bify
+        .plugin(tsify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source("app.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init(config.ext.sourceMapsConfig))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/js"))
+
+});
+
+
+gulp.task("copy-html", () => {
+    return gulp.src(paths.htmlSource)
+        .pipe(gulp.dest("dist/views"))
+});
+
+gulp.task("copy-css", () => {
+    return gulp.src(paths.cssSource)
+        .pipe(gulp.dest("dist/css"))
 });
 
 // gulp.task("default", gulp.series("clean", "run-tests", "compile", (done) => {
@@ -39,7 +112,7 @@ gulp.task("compile", () => {
 //     done();
 // }));
 
-gulp.task("default", gulp.series("clean", "compile", (done) => {
+gulp.task("default", gulp.series("clean", "compile", "copy-html", "copy-css", (done) => {
     console.log("GULP: default task running...");
     done();
 }));
