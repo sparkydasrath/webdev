513ca75eee713c0bf9d8914e767cb1f5d565d9c5
sparky.dasrath <sdasrath@gmail.com>
2018-11-23 22:01:59 -0500

    The Complete Node.js Developer Course (2nd Edition) - fundamentals
    - refactoring

--- node/complete_njs/noteappv1/app.js
+++ node/complete_njs/noteappv1/app.js
@@ -10,7 +10,8 @@ const argv = yargs.argv;
 console.log("Process ", process.argv);
 console.log("Args ", argv);
 
-let command = process.argv[2];
+// let command = process.argv[2];
+let command = argv._[0];
 
 if (command === "add") {
     notes.addNote(argv.title, argv.body);
--- node/complete_njs/noteappv1/notes.js
+++ node/complete_njs/noteappv1/notes.js
@@ -1,14 +1,49 @@
 console.log("starting notes");
+const fs = require("fs");
+
+let fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync("notes-data.json");
+        return JSON.parse(notesString);
+    } catch (error) {
+        return [];
+    }
+}
+
+let saveNotes = (notes) => {
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+}
 
 let addNote = (title, body) => {
-    console.log("adding note", title, body);
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    }
+
+    let duplicateNotes = notes.filter(note => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+    }
 };
 
 let getAll = () => {
-    console.log("Getting all notes");
+    return notes;
+}
+
+let getNote = (title) => {
+
+}
+
+let removeNote = (title) => {
+
 }
 
 module.exports = {
     addNote,
+    getNote,
+    removeNote,
     getAll,
 }
