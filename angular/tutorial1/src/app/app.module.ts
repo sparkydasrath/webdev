811decb8324ad26749fad370c459b2134321101f
Sparky Dasrath <sdasrath@gmail.com>
2019-04-16 13:38:37 -0400

    working through the tutorial - master/detail

--- angular/tutorial1/src/app/app.module.ts
+++ angular/tutorial1/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
