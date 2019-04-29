0b52746409b1734f1be8eec76b16df184644ffa9
Sparky Dasrath <sdasrath@gmail.com>
2019-04-28 22:21:37 -0400

    asp.net mvc basic tutorial

---
+++ asp/aspTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspTutorial.Controllers
+{
+    // [Route("api/[controller]")] [ApiController]
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            // GET: /HelloWorld/
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            // GET: /HelloWorld/Welcome/
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NUmTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
